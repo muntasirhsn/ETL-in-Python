47e3e18e76872bfd8e6870d09f095bcda63dbdcb
Muntasir Hossain <muntasir.phy@gmail.com>
2023-06-03 00:48:58 +1000

    Update base.py

--- scripts/base.py
+++ scripts/base.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, Session
 
 # Create the engine
-engine = create_engine("postgresql://user:password@@localhost/sample_db") 
+engine = create_engine("postgresql://user:password@@localhost/sample_db") # use your own database details
 # Initialize the session
 session = Session(engine)
 
